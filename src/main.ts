e0b6c98fc685e5d87d84e2fe6cec21fd0651248a
Jonas Franke <jonasfranke@sdevs.org>
2024-12-20 20:32:48 +0100

    Improve debug output in main function to display tokens before parsing

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ function main(sourceCode: string): void {
     const lexer = new Lexer(sourceCode);
     const tokens: Token[] = lexer.tokenize();
 
+    console.log("Tokens:\n" + JSON.stringify(tokens, null, 2));
+
     const parser = new Parser(tokens);
     const ast = parser.parse();
 
-    console.log(JSON.stringify(ast, null, 2));
+    console.log("\nAST:\n" + JSON.stringify(ast, null, 2));
 
     const interpreter = new Interpreter();
     interpreter.interpret(ast);
