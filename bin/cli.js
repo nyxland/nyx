dae1e099cba47bbb0a57bc040844a4f9f6f86eb0
Jonas Franke <jonasfranke@sdevs.org>
2024-12-20 20:27:08 +0100

    Refactor CLI to use Node.js built-in modules and update package.json for executable

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const { main } = require('../src/main');
+const fs = require('node:fs');
+const path = require('node:path');
+const { main } = require('../build/main');
 
 const args = process.argv.slice(2);
 
